f1ec12c425fd768182108b1c09f5835fcf844b8a
Sanchit Garg <sanchitgarg2@gmail.com>
2018-03-30 11:20:40 +0530

    Changing Bill, Person and meter logicclass diagram cycle

---
+++ app/src/main/java/gtbit/retro06/www/sikkimelectricityboard/GridDataGenerator.java
@@ -0,0 +1,22 @@
+package gtbit.retro06.www.sikkimelectricityboard;
+
+import com.google.maps.model.LatLng;
+
+public class GridDataGenerator {
+
+	public static void main(String[] args) {
+	LatLng[] SIKKIM_BOPUNDARIES = {
+			new LatLng(27.3696,88.0481),
+			new LatLng(27.5792,88.0920),
+			new LatLng(27.7762,88.1964),
+			new LatLng(27.9413,88.1442),
+			new LatLng(27.9631,88.3283),
+			new LatLng(28.1074,88.6331),
+			new LatLng(28.0711,88.7567),
+			new LatLng(27.8721,88.8803),
+			new LatLng(27.5536,88.7787),
+			new LatLng(27.3104,88.9188),
+			new LatLng(27.3153,88.5535),
+			new LatLng(27.3567,88.2980)};	}
+
+}
