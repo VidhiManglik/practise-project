ab5fc788c19b358054a8f55969fbb1123e8ac71f
Taranjot Singh <taranzz25@gmail.com>
2018-03-30 17:52:20 +0530

    Added Tips

--- app/src/main/java/gtbit/retro06/www/sikkimelectricityboard/VerticalListAdapter.java
+++ app/src/main/java/gtbit/retro06/www/sikkimelectricityboard/VerticalListAdapter.java
@@ -1,31 +1,37 @@
 package gtbit.retro06.www.sikkimelectricityboard;
 
+/**
+ * Created by HPG6 on 30-03-18.
+ */
+
 import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class HorizontalListAdapter extends RecyclerView.Adapter<HorizontalListAdapter.ViewHolder> {
+public class VerticalListAdapter extends RecyclerView.Adapter<VerticalListAdapter.ViewHolder> {
 
     private Activity activity;
 
-    public HorizontalListAdapter(Activity activity) {
+    public VerticalListAdapter(Activity activity) {
         this.activity = activity;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = activity.getLayoutInflater();
-        View view = inflater.inflate(R.layout.item_horizontal_list, viewGroup, false);
+        View view = inflater.inflate(R.layout.item_vertical_list, parent, false);
 
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(HorizontalListAdapter.ViewHolder viewHolder, final int position) {
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
         viewHolder.linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,12 +49,12 @@ public class HorizontalListAdapter extends RecyclerView.Adapter<HorizontalListAd
      * View holder to display each RecylerView item
      */
     protected class ViewHolder extends RecyclerView.ViewHolder {
-
         private LinearLayout linearLayout;
 
         public ViewHolder(View view) {
             super(view);
-            linearLayout = view.findViewById(R.id.layout);
+            linearLayout = view.findViewById(R.id.layout2);
         }
+
     }
 }
